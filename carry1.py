19d93ec2fe777a1d3b1160391ed9da879770910d
O0oTGo0O <73126354+O0oTGo0O@users.noreply.github.com>
2023-07-01 14:27:34 +0800

    Update carry1.py

--- carry1.py
+++ carry1.py
@@ -1,1 +1,23 @@
-
+import requests
+import json
+def handle(req):
+    listener_ip = '166.111.73.41'
+    database_ip = '166.111.73.41'
+    openfaas_ip = 'http://166.111.73.41:31112/function/openfaas'
+    data = {
+        "ip": listener_ip,
+        "device": "magician",
+        "method": "suck on",
+        "user": "xuwt",
+        "pwd": "123456"
+    }
+    response1 = requests.get(url=openfaas_ip, data=json.dumps(data))
+    data = {
+        "ip": listener_ip,
+        "device": "magician",
+        "method": "suck off",
+        "user": "xuwt",
+        "pwd": "123456"
+    }
+    response2 = requests.get(url=openfaas_ip, data=json.dumps(data))
+    return response1.text + '\n' + response2.text
